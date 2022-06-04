ce823ec31d4b7b47d81380474d77cb8fd58b4c77
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-06-04 00:04:08 -0400

    implemented network-only fetch policy for useQuery

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,21 +1,20 @@
-import React, { useEffect } from 'react';
+import React, {useState} from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useLazyQuery, useMutation } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 
 import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
 import { QUERY_ME } from '../utils/graphql/queries';
 import { DELETE_BOOK } from '../utils/graphql/mutations';
+import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
+  
+  // fetch-only to force re-rendering in case of added/deleted books
+  const { loading, error, data } = useQuery(QUERY_ME, { fetchPolicy: 'network-only' }); 
+  const [ deleteBook, { delBkEr, delBkData} ] = useMutation(DELETE_BOOK);  
+  const [ test, setTest ] = useState('test');
 
-  const [ queryMe, { called, loading, error, data } ] = useLazyQuery(QUERY_ME);
-  const [ deleteBook, { delBkEr, delBkData} ] = useMutation(DELETE_BOOK);
-  
-  useEffect(() => {
-    queryMe();
-  }, []);
-  
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -33,18 +32,21 @@ const SavedBooks = () => {
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
       // also update the page:
-      queryMe();
+      setTest('doese this work?')
     } catch (err) {
       console.error(err);
     }
   };
-  
 
   if (error) {
     return (<div>
       <h2>ERROR ACCESSING YOUR ACCOUNT...</h2>;
       <p>{JSON.stringify(error)}</p>
     </div>)
+  }
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
   }
 
   if (data) {
@@ -62,28 +64,24 @@ const SavedBooks = () => {
               : 'You have no saved books!'}
           </h2>
           <CardColumns>
-            {data.me.savedBooks.map((book) => {
-              return (
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              );
-            })}
+            {data.me.savedBooks.map( book => 
+              <Card key={book.bookId} border='dark'>
+                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className='small'>Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
+             )}
           </CardColumns>
         </Container>
       </>
     );
   }
-  
-  return <h2>LOADING...</h2>;
 
 };
 
